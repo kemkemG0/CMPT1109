4267fbe9135182501e864a818834dc2831f60fd9
kenshin <tanaka.aa@icloud.com>
2021-09-08 11:31:19 -0700

    first commit

---
+++ a.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include "atcoder/all"
+#define REP(i,n) for(ll i=0;i<ll(n);++i)
+#define REPD(i,n) for(ll i=ll(n-1);i>=0;--i)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);++i)
+#define FORD(i,b,a) for(ll i=b;i>=ll(a);--i)
+#define ALL(x) x.begin(),x.end()
+
+#define INF (ll)(1LL<<30)
+#define LLINF (ll)(1LL<<60)
+#define MOD (ll)(1e9+7)
+#define PI 3.141592653589
+
+#define YESNO(T) cout<<((T)?"YES":"NO")<<endl;
+#define yesno(T) cout<<((T)?"yes":"no")<<endl;
+#define YesNo(T) cout<<((T)?"Yes":"No")<<endl;
+
+template <typename T> T &chmin(T &a, const T &b) { return a = min(a, b); }
+template <typename T> T &chmax(T &a, const T &b) { return a = max(a, b); }
+
+#define DDD fixed<<setprecision(10)
+#define endl "\n"
+
+#define __MAGIC__ ios::sync_with_stdio(false);cin.tie(nullptr);
+
+using ll= long long;
+using namespace std;
+using namespace atcoder;
+using mint = modint1000000007;
+/*..................DEFINE GLOBAL VARIABLES...................*/
+int dh[]={0,1,0,-1};
+int dw[]={1,0,-1,0};
+
+/*.....................DEFINE FUNCTIONS ......................*/
+
+
+
+/*.........................kemkemG0...........................*/
+signed main() {
+    __MAGIC__
+
+
+    
+
+    
+
+
+
+    
+
+    return 0;
+}
