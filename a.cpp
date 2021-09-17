1d97f006e3c208cf99af9cdb91e1ca7275431f24
kenshin <tanaka.aa@icloud.com>
2021-09-17 11:54:45 -0700

    initial

--- a.cpp
+++ a.cpp
@@ -1,52 +1,21 @@
-#include<bits/stdc++.h>
-#include "atcoder/all"
-#define REP(i,n) for(ll i=0;i<ll(n);++i)
-#define REPD(i,n) for(ll i=ll(n-1);i>=0;--i)
-#define FOR(i,a,b) for(ll i=a;i<=ll(b);++i)
-#define FORD(i,b,a) for(ll i=b;i>=ll(a);--i)
-#define ALL(x) x.begin(),x.end()
+/*
+** This program finds the roots of a quadratic equation like
+** ax^2 + bx + c = 0. Using math, the two roots are fourmlated
+** as follows:
+** x1 = (-b + squareRoot(b^2 - 4ac)) / 2a
+** x2 = (-b - squareRoot(b^2 - 4ac)) / 2a
+*/
 
-#define INF (ll)(1LL<<30)
-#define LLINF (ll)(1LL<<60)
-#define MOD (ll)(1e9+7)
-#define PI 3.141592653589
-
-#define YESNO(T) cout<<((T)?"YES":"NO")<<endl;
-#define yesno(T) cout<<((T)?"yes":"no")<<endl;
-#define YesNo(T) cout<<((T)?"Yes":"No")<<endl;
-
-template <typename T> T &chmin(T &a, const T &b) { return a = min(a, b); }
-template <typename T> T &chmax(T &a, const T &b) { return a = max(a, b); }
-
-#define DDD fixed<<setprecision(10)
-#define endl "\n"
-
-#define __MAGIC__ ios::sync_with_stdio(false);cin.tie(nullptr);
-
-using ll= long long;
-using namespace std;
-using namespace atcoder;
-using mint = modint1000000007;
-/*..................DEFINE GLOBAL VARIABLES...................*/
-int dh[]={0,1,0,-1};
-int dw[]={1,0,-1,0};
-
-/*.....................DEFINE FUNCTIONS ......................*/
-
-
-
-/*.........................kemkemG0...........................*/
-signed main() {
-    __MAGIC__
-
-
-    
-
-    
-
-
-
-    
-
-    return 0;
+using namespace std int main() {
+  double a = 2, b = 4, c = 2;
+  int x1, x2;
+  double delta = b * b - 4 * a * c;
+  x1 = (-b + pow(delta, 0.5)) / 2 * a;
+  x2 = (-b - pow(delta, 0.5)) / 2 * a;
+  cout << "Roots of " << a << "x^2 + "  // Print the result
+       << b << "x + " << c << "= 0 are:" << endl;
+  cout << "x1 = " << x1 << "\n";
+  cout << "x2 = " << x2 << "\n";
+  cout << "Delta is " << delta << "\n";
+  return;
 }
