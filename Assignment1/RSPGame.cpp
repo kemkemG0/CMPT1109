2bfcbfe92bc99c3d62ee785e95fd0411a858d654
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-10-05 16:16:55 -0700

    RSPGame

---
+++ Assignment1/RSPGame.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<random>
+
+using namespace std;
+
+const int R = 0;
+const int S = 1;
+const int P = 2;
+string hands[3]={"rock","scissor","paper"};
+
+int main() {
+    // init vars
+    int user=0;
+    int enem = rand()%3;
+    // user input
+    cout<<"Select rock (0), scissor (1), or paper (2): ";
+    cin >> user;
+    // common output
+    cout<<"The computer is "<< hands[enem] <<". You are "<< hands[user];
+    // Draw
+    if(user==enem){
+        cout<<" too. It is a draw." << endl;
+        return 0;
+    }
+    // Won
+    if((user==R && enem==S) || (user==S && enem==P) || (user==P && enem==R)){
+        cout<<". You won !"<<endl;
+        return 0;
+    }
+    // Lost
+    cout<<". You lost !"<<endl;
+    return 0;
+}
