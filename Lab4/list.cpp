0d3095bd55b6336b95ee736ad2bdd69be8250e23
kenshin <tanaka.aa@icloud.com>
2021-10-08 12:08:30 -0700

    small fix

--- Lab4/list.cpp
+++ Lab4/list.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     // declare and init vars
     double price, tax, item_price, total_amount = 0;
-    stringstream ss;// string stream
+    stringstream ss;
     char yn = 'y';
 
     for(int item=1; yn=='y'; ++item){
@@ -22,18 +22,17 @@ int main(){
         item_price = price*(1+tax*0.01);
         total_amount += item_price;
         // format and store values in ss
-        ss << right << setw(2) << item;
-        ss << right << setw(12) <<  fixed << setprecision(2) << price;
-        ss << right << setw(9) << tax << "%";
-        ss << right << setw(13) << item_price << endl;
-        if(yn != 'y' && yn != 'n'){
-            cout << "Invalid input" << endl;
-            return 0;
-        }
+        ss << right;
+        ss << setw(2) << item;
+        ss << setw(12) <<  fixed << setprecision(2) << price;
+        ss << setw(9) << tax << "%";
+        ss << setw(13) << item_price << endl;
     }
+    if(total_amount > 1000) total_amount *= 0.97;
 
     // output
     cout << "\nItem   " << "Price   " << "Tax Rate%   " << "Item Price" << endl;
     cout << "=====================================" << endl;
-    cout<< ss.str()<<endl;    
+    cout<< ss.str()<<endl;
+    cout << "Total Amount: " << fixed << setprecision(2) << total_amount << "$" << endl;
 }
