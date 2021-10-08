dcf3d03c38247f9734441a53dcf3f0af3652ec9e
kenshin <tanaka.aa@icloud.com>
2021-10-08 12:01:49 -0700

    use sstream

--- Lab4/list.cpp
+++ Lab4/list.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     // declare and init vars
     double price, tax, item_price, total_amount = 0;
-    string values_list_str = "";// store values with space seprate
+    stringstream ss;// string stream
     char yn = 'y';
 
     for(int item=1; yn=='y'; ++item){
@@ -21,31 +21,19 @@ int main(){
         // calc
         item_price = price*(1+tax*0.01);
         total_amount += item_price;
-        // store values
-        values_list_str += to_string(item) + " " + to_string(price) + " " + to_string(tax) + " " + to_string(item_price) + " ";
-        //validation
+        // format and store values in ss
+        ss << right << setw(2) << item;
+        ss << right << setw(12) <<  fixed << setprecision(2) << price;
+        ss << right << setw(9) << tax << "%";
+        ss << right << setw(13) << item_price << endl;
         if(yn != 'y' && yn != 'n'){
             cout << "Invalid input" << endl;
             return 0;
         }
     }
 
-    // common output
+    // output
     cout << "\nItem   " << "Price   " << "Tax Rate%   " << "Item Price" << endl;
     cout << "=====================================" << endl;
-    
-    string value = "";
-    stringstream ss{values_list_str};
-    int cnt = 0;// takes [0-3]
-    while(getline(ss, value, ' ')){
-        // output each columns with nice format
-        if(cnt==0)cout << right << setw(2) << value;
-        if(cnt==1)cout << right << setw(12) << fixed << setprecision(2) << stod(value);
-        if(cnt==2)cout << right << setw(9) << stoi(value) << "%";
-        if(cnt==3)cout << right << setw(13) << stod(value) << endl;
-
-        cnt = (cnt+1)%4;
-    }
-    if(total_amount > 1000) total_amount *= 0.97;
-    cout << "\n\nTotal Amount: " << total_amount << "$" << endl;
+    cout<< ss.str()<<endl;    
 }
