24ba83d1816725b5f207c62ee645a85428295c35
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-11-30 18:29:45 -0800

    modify

--- Lab10/testProgram.cpp
+++ Lab10/testProgram.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int binToDecimal(const string binaryString);
+int binToDecimal(const string& binaryString);
 
 int main(){
-    cout<< binToDecimal("10")<<endl;
+    string S = "1111111111";
+    cout<< binToDecimal(S)<<endl;
     return 0;
 }
 
-int binToDecimal(const string binaryString){
-    int ret = 0;
+int binToDecimal(const string& binaryString){
     int sz = binaryString.size();
     if(!sz)return 0;
-    ret=(binaryString[0]-'0')*pow(2,sz-1) + binToDecimal(binaryString.substr(1));
-    return ret;
+    // str[0] + binToDecimal(str[1:]) :=   now + next
+    return (binaryString[0]-'0')*pow(2,sz-1) + binToDecimal(binaryString.substr(1));
 }
